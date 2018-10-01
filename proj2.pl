2ec33b44ae958717cee4a965accce22fbd5f6871
Adam Quigley <adamjohnquigley@gmail.com>
2018-10-01 23:38:14 +1000

    Add get_filled_chars predicate

--- proj2.pl
+++ proj2.pl
@@ -8,6 +8,8 @@
 % Author: Adam Quigley
 % Date created: 30.09.2018
 
+:- ensure_loaded(library(clpfd)).
+
 main(PuzzleFile, WordlistFile, SolutionFile) :-
 	read_file(PuzzleFile, Puzzle),
 	read_file(WordlistFile, Wordlist),
@@ -63,11 +65,37 @@ valid_puzzle([Row|Rows]) :-
 	maplist(same_length(Row), Rows).
 
 
-% solve_puzzle(Puzzle0, WordList, Puzzle)
 % should hold when Puzzle is a solved version of Puzzle0, with the
 % empty slots filled in with words from WordList.  Puzzle0 and Puzzle
 % should be lists of lists of characters (single-character atoms), one
 % list per puzzle row.  WordList is also a list of lists of
 % characters, one list per word.
 
-solve_puzzle(Puzzle, _, Puzzle).
+%solve_puzzle(Puzzle, _, Puzzle).
+
+solve_puzzle(Puzzle0, WordList, Puzzle) :-
+	Puzzle = Puzzle0.
+
+
+% Holds when the a slot exists to be filled in the puzzle.
+% Slot will be a maximal horizontal or vertical sequence of 
+% fill-able and pre-filled squares within the puzzle.
+select_slot(Puzzle, Slot) :-
+	length(Puzzle, L),
+	Slot = L.
+
+
+% Filters a slot to give a list of characters already filled
+get_filled_chars(SlotList, FilledCharList) :-
+    include(is_alpha, SlotList, FilledCharList).
+
+% 
+%get_valid_words(Slot, Wordlist, ValidWordList) :-
+%	get_filled_chars(Slot, FilledCharList),
+%	subset(FilledCharList, WordList)
+%% also test for length.
+
+% bind_terms([], _, []).
+% bind_terms(_, [], []).
+% bind_terms(LogicalVariables, TermList, BoundTermList) :-
+%  	(LogicalVariables, TermList, x).
