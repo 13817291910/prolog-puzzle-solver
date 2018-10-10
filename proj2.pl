4d7a8d5c46c022cc2a5e36ee38d83f597ed81765
Adam Quigley <adam.quigley@twobulls.com>
2018-10-10 18:42:13 +1100

    Update module documentation

--- proj2.pl
+++ proj2.pl
@@ -2,30 +2,33 @@
 % Author: Adam Quigley
 % Last updated: 09.10.2018
 %
-% Program for solving Fill-it-in style puzzles
+% Program for solving Fill-In style puzzles
 % COMP30020 Declarative Programming, 2018 Semester 2, Project 2
 % The University of Melbourne, School of Computing and Information Systems
 % 
-% This module solves fillin (fill-it-in) style puzzles by constructing a
+% This module solves Fill-In (fill-it-in) style puzzles by constructing a
 % representation of the puzzle at a list of 'slots', and repeatedly
 % trying valid words from the provided word list until it finds a 
 % solution. Once found, the solution is written to a solution file.
 % 
-% The strategy is as follows. The program reads in a puzzle file and
-% creates a list of 'Slots' by aggregating series of non-black ('#') 
-% squares of length greater than one, and sorts the list of slots.
-% It then proceeds to match words with slots. If there is a word 
-% that uniquely matches a slot it will be immediately unified with that 
-% slot, otherwise the largest slot is selected and unified with valid 
-% matching word. If no solution is found, the program backtracks and 
-% tries different combinations of words and slots until the puzzle 
-% is solved. 
+% The program reads in a puzzle file and creates a list of 'slots' by 
+% aggregating series of non-black ('#') squares of length greater than one. 
+% It sorts the list of slots and the list of words, and then proceeds to 
+% match words with slots. If there is a word that uniquely matches a slot 
+% it will be immediately unified with that slot, otherwise the largest slot
+% is selected and unified with valid  matching word. 
+% To narrow the possible search space, the program sorts the slots by 
+% number of possible unifications with words, then by length. 
+% This ensures that the longest words with the fewest possible matches 
+% amongst words of their size are picked first. If no solution is found, 
+% the program backtracks and tries different combinations of words 
+% and slots until the puzzle is solved.
 % 
 % Usage:
-% Call program with main(PuzzleFile, WordlistFile, SolutionFile) where
-% PuzzleFile 	- the dir location of a valid puzzle file
-% WordlistFile 	- the dir location of a list of words
-% SolutionFile 	- the dir location of a file to write a solution to
+% Call program with main(PuzzleFile, WordlistFile, SolutionFile) where:
+% PuzzleFile    - the dir location of a valid puzzle file
+% WordlistFile  - the dir location of a list of words
+% SolutionFile  - the dir location of a file to write a solution to
 
 :- ensure_loaded(library(clpfd)).
 :- use_module(library(pairs)).
